2acc86a3bd13293950c702dbe892c67376479045
주동호 <100278301+JOODON@users.noreply.github.com>
2024-01-29 09:40:56 +0900

    Delete FindJobProject directory
    변경 사항으로 인한 삭제

--- FindJobProject/src/main/kotlin/com/example/findjobproject/repository/MajorCategoryRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.findjobproject.repository
-
-import com.example.findjobproject.entitty.category.MajorCategory
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MajorCategoryRepository : JpaRepository<MajorCategory, Long>{
-}
