c70a989a9726ef40b20bc0678f0c9d083ab3fa38
jwo1024 <dnemtmxlr@gmail.com>
2024-02-14 02:03:34 +0900

    desgin: home card slider 디자인에 맞게 구현

---
+++ finejob-fe/src/components/CardSlider.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import styles from "@/styles/_cardslider.module.scss";
+import {
+  useEffect,
+  useState,
+  useRef,
+  ReactNode,
+  useCallback,
+  useMemo,
+} from "react";
+
+export default function CardSlider({
+  children,
+  scrollSize,
+  scrollCardNumber = 1,
+  gap = 20,
+  width,
+  height,
+}: {
+  children: ReactNode[];
+  scrollSize?: number;
+  scrollCardNumber?: number;
+  gap?: number;
+  width?: number;
+  height?: number;
+}) {
+  const [isFirstCard, setIsFirstCard] = useState(true);
+  const [isLastCard, setIsLastCard] = useState(false);
+
+  const cardContainerRef = useRef<HTMLDivElement>(null);
+
+  const cardRefs = useRef(
+    Array(children.length)
+      .fill(null)
+      .map(() => useRef<HTMLDivElement>(null))
+  );
+
+  const withStyle = {
+    width: width ? width : "100%",
+    height: height ? height : "100%",
+  };
+
+  const cardWidth = 48; // rem
+  const defaultgap = 1;
+  const defaultScrollSize = cardWidth + defaultgap;
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  // let currentSlide = 0;
+
+  const showSlide = (slideIndex: number) => {
+    // console.log("slideIndex", slideIndex);
+    cardRefs.current.forEach((cardRef, index) => {
+      if (index === slideIndex) {
+        // console.log(cardRef.current!.classList);
+        cardRef.current!.classList.add(styles.activeCard);
+        cardRef.current!.classList.remove(styles.deactiveCard);
+      } else {
+        cardRef.current!.classList.remove(styles.activeCard);
+        cardRef.current!.classList.add(styles.deactiveCard);
+      }
+    });
+
+    setCurrentSlide(slideIndex);
+    let currentScrollSize = cardWidth / 2 + defaultScrollSize * slideIndex;
+    cardContainerRef.current!.style.transform = `translateX(-${currentScrollSize}rem)`;
+
+    if (slideIndex <= 0) setIsFirstCard(true);
+    else setIsFirstCard(false);
+    if (slideIndex >= children.length - 1) setIsLastCard(true);
+    else setIsLastCard(false);
+  };
+
+  const handleLeftClick = () => {
+    if (isFirstCard) return;
+    showSlide(currentSlide - 1);
+  };
+
+  const handleRightClick = () => {
+    if (isLastCard) return;
+
+    showSlide(currentSlide + 1);
+  };
+
+  return (
+    <section style={withStyle} className={styles.cardSlider}>
+      {/* Card Container */}
+      <div className={styles.cardContainer} ref={cardContainerRef}>
+        {children?.map((child, index) => {
+          if (index === 0) {
+            return (
+              <article
+                key={index}
+                className={styles.activeCard}
+                ref={cardRefs.current[index]}
+              >
+                {child}
+              </article>
+            );
+          }
+          return (
+            <article
+              key={index}
+              className={styles.deactiveCard}
+              ref={cardRefs.current[index]}
+            >
+              {child}
+            </article>
+          );
+        })}
+      </div>
+      {/* Left Button */}
+      {!isFirstCard && (
+        <button className={styles.leftButton} onClick={handleLeftClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+        </button>
+      )}
+      {/* Right Button */}
+      {!isLastCard && (
+        <button className={styles.rightButton} onClick={handleRightClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      )}
+    </section>
+  );
+}
--- finejob-fe/src/containers/main/HomeBanner.tsx
+++ finejob-fe/src/containers/main/HomeBanner.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
+import CardSlider from "@/components/CardSlider";
 import "swiper/scss";
 import "swiper/scss/pagination";
 import "@/styles/customSwiperNavigation.scss";
 import styles from "@/styles/HomeBanner.module.scss";
+import Image from "next/image";
 
 export default function HomeBanner() {
   return (
     <section className={styles.homeBannerContainer}>
-      <div className={styles.homeBanner}>
-        <Swiper
+      {/* <div className={styles.homeBanner}> */}
+      {/* <Swiper
           slidesPerView={1.5}
           centeredSlides={true}
           spaceBetween={100}
@@ -30,15 +32,32 @@ export default function HomeBanner() {
           <SwiperSlide>
             <BannerCard />
           </SwiperSlide>
-        </Swiper>
-      </div>
+        </Swiper> */}
+      <CardSlider scrollCardNumber={1.5} gap={100}>
+        {/* <EmptyCard /> */}
+        <BannerCard />
+        <BannerCard />
+        <BannerCard />
+        <BannerCard />
+        {/* <EmptyCard /> */}
+      </CardSlider>
     </section>
   );
+}
+
+function EmptyCard() {
+  return <div className={styles.bannerCard}></div>;
 }
 
 function BannerCard() {
   return (
     <div className={styles.bannerCard}>
+      <Image
+        src="/svg/FineJob-logo-horizontal.svg"
+        alt="ribbon"
+        width={120}
+        height={100}
+      />
       <h2>
         <strong>파인잡</strong>으로 취업도 하고 <br />
         <strong>합격 축하금</strong>도 받자!
