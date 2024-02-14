24710d808027a12ba2d3a6616682040725477707
주동호 <100278301+JOODON@users.noreply.github.com>
2024-02-14 10:20:23 +0900

    Update AdminController.kt
    카테고리 데이터 추가 기능 컨트롤러 작업

--- FindJobProject/src/main/kotlin/com/example/findjobproject/controller/AdminController.kt
+++ FindJobProject/src/main/kotlin/com/example/findjobproject/controller/AdminController.kt
@@ -34,7 +34,9 @@ class AdminController(
     fun saveAnnouncement(){
         announcementService.saveAnnouncementList(exAnnouncementList)
     }
-
-
-
+    
+    @GetMapping("/category/save")
+    fun saveCategory(){
+        categorySaveService.saveCategoryAllData()
+    }
 }
