46a8d71cf37aa617b53253800322211c7e1340e0
jwo1024 <dnemtmxlr@gmail.com>
2024-04-24 22:54:36 +0900

    fix: NavBar 목록버튼 url의 pathName과 연동

--- finejob-fe/src/containers/home/NavBar.tsx
+++ finejob-fe/src/containers/home/NavBar.tsx
@@ -2,7 +2,8 @@
 
 import Link from "next/link";
 import styles from "@/styles/container-home/NavBar.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
   const activeClassName = styles.active;
@@ -15,6 +16,17 @@ export default function NavBar() {
     false,
   ]);
 
+  const pathName = usePathname();
+
+  useEffect(() => {
+    console.log(pathName);
+    if (pathName === "/") setActive([false, true, false, false, false, false]);
+    else if (pathName === "/job-search")
+      setActive([true, false, false, false, false, false]);
+    else if (pathName === "/job-suitability")
+      setActive([false, false, false, false, false, true]);
+  }, [pathName]);
+
   return (
     <nav className={styles.navBar}>
       <ul>
@@ -22,44 +34,27 @@ export default function NavBar() {
           <Link
             href="/job-search"
             className={active[0] ? activeClassName : undefined}
-            onClick={() => setActive([true, false, false, false, false, false])}
           >
             채용정보
           </Link>
         </li>
         <li>
-          <Link
-            href="/"
-            className={active[1] ? activeClassName : undefined}
-            onClick={() => setActive([false, true, false, false, false, false])}
-          >
+          <Link href="/" className={active[1] ? activeClassName : undefined}>
             신입 인턴
           </Link>
         </li>
         <li>
-          <Link
-            href="/"
-            className={active[2] ? activeClassName : undefined}
-            onClick={() => setActive([false, false, true, false, false, false])}
-          >
+          <Link href="/" className={active[2] ? activeClassName : undefined}>
             헤드헌팅
           </Link>
         </li>
         <li>
-          <Link
-            href="/"
-            className={active[3] ? activeClassName : undefined}
-            onClick={() => setActive([false, false, false, true, false, false])}
-          >
+          <Link href="/" className={active[3] ? activeClassName : undefined}>
             기업 연봉
           </Link>
         </li>
         <li>
-          <Link
-            href="/"
-            className={active[4] ? activeClassName : undefined}
-            onClick={() => setActive([false, false, false, false, true, false])}
-          >
+          <Link href="/" className={active[4] ? activeClassName : undefined}>
             커리어
           </Link>
         </li>
@@ -67,10 +62,8 @@ export default function NavBar() {
           <Link
             href="/job-suitability"
             className={active[5] ? activeClassName : undefined}
-            onClick={() => setActive([false, false, false, false, false, true])}
           >
-            {/* 지원 적합도(tmp) */}
-            커뮤니티
+            지원 적합도
           </Link>
         </li>
       </ul>
